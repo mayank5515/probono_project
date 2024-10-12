16b5c816f697b29412b6a289c63b7f247e57802f
Mayank Chaturvedi <chaturvedimayank70@gmail.com>
2024-10-12 19:57:48 +0530

    ALMOST FINISHED RESPONSIVE MOBILE NAV

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const navlinksArr = [
   },
   {
     linkTitle: "Contact Us",
-    linkPath: "/contactUs",
+    linkPath: "/contact",
   },
   {
     linkTitle: "Our Team",
@@ -23,7 +23,7 @@ const navlinksArr = [
 ];
 
 export default function Header() {
-  // const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
+  const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
   const [isDropDownOpen, setIsDropDownOpen] = useState(false);
   // UGLY: REFACTOR YOUR CODE
 
@@ -34,7 +34,7 @@ export default function Header() {
   return (
     <nav className=" px-6 bg-[#202020] text-white top-0 sticky z-50 w-full py-2">
       {/*  NOTE: OUTER MOST DIV */}
-      <div className="flex items-center justify-evenly ">
+      <div className="flex flex-wrap lg:flex-nowrap items-center justify-around border-2 border-green-500 ">
         {/* NAME AND LOGO */}
         <div className="flex items-center space-x-2 md:space-x-4">
           <Link to="/">
@@ -48,23 +48,43 @@ export default function Header() {
             <Link to="/">NYAYASARTHAK</Link>
           </h1>
         </div>
+        <button className="lg:hidden">
+          <RxHamburgerMenu size={25} />
+        </button>
         {/* NOTE: LINKS */}
-        <div className=" flex items-center  space-x-4 p-2 font-[Raleway] text-[16px] font-semibold">
+        <div className=" w-full lg:w-auto lg:flex-row   flex flex-col   space-y-2  lg:items-center border-2 border-orange-400 lg:space-x-4 lg:space-y-0 p-2 font-[Raleway] text-[16px] font-semibold">
           {navlinksArr.map((el, i) => {
             if (el.linkTitle === "About") {
               return (
                 <div key={i}>
-                  <div className=" font-medium hover:text-[#e1dcd6] flex items-center">
+                  <div className=" font-medium  border-2 border-purple-400  hover:text-[#e1dcd6] flex items-center">
                     <Link to={el.linkPath}>{el.linkTitle}</Link>
                     <button onClick={() => setIsDropDownOpen(!isDropDownOpen)}>
                       <IoMdArrowDropdown className="cursor-pointer hover:scale-125" />
                     </button>
                   </div>
+                  {/* <div
+                    className={`${
+                      isDropDownOpen ? "visible" : "hidden"
+                    } border-[2px] border-gray-500 rounded-md space-y-1 my-2 p-1 `}
+                  >
+                    <div className="hover:text-[#e1dcd6]">
+                      <Link to="/about">Who We Are</Link>
+                    </div>
+                    <hr className="border-t border-gray-500" />
+                    <div className="hover:text-[#e1dcd6]">
+                      <Link to="/about/whatwedo">What We Do</Link>
+                    </div>
+                    <hr className="border-t border-gray-500" />
+                    <div className="hover:text-[#e1dcd6]">
+                      <Link to="/about/faqs">FAQS</Link>
+                    </div>
+                  </div> */}
                   {/* DROPDOWN FOR ABOUT */}
                   <section
                     className={`${
                       isDropDownOpen ? "visible" : "hidden"
-                    } z-50 border-2 border-gray-700 text-center  p-2 w-[10rem] translate-y-2  -translate-x-10 absolute bg-[#202020]`}
+                    } border-[2px] border-gray-500 rounded-md space-y-1 my-2 p-1  lg:z-50 lg:border-2 lg:border-gray-700 lg:text-center  lg:p-2 lg:w-[10rem] lg:translate-y-2  lg:-translate-x-10 lg:absolute bg-[#202020]`}
                   >
                     <div className="hover:text-[#e1dcd6]">
                       <Link to="/about">Who We Are</Link>
@@ -84,7 +104,7 @@ export default function Header() {
             if (el.linkTitle === "Our Team") {
               return (
                 <button
-                  className=" rounded-md bg-[#eed814] hover:bg-[#afa016] transition-colors duration-1000 px-2 py-1 text-black text-[14px] font-bold"
+                  className="hidden lg:block border-2 border-purple-400  rounded-md bg-[#eed814] hover:bg-[#afa016] transition-colors duration-1000 px-2 py-1 text-black text-[14px] font-bold"
                   key={i}
                 >
                   <Link to={el.linkPath}>{el.linkTitle}</Link>
@@ -92,14 +112,17 @@ export default function Header() {
               );
             }
             return (
-              <div className="hover:text-[#e1dcd6]" key={i}>
+              <div
+                className="hover:text-[#e1dcd6] border-2 border-purple-400 "
+                key={i}
+              >
                 <Link to={el.linkPath}>{el.linkTitle}</Link>
               </div>
             );
           })}
 
           {/* SOCIAL MEDIA LINKS IN ONE DIV */}
-          <div className="flex items-center  space-x-4">
+          <div className="flex items-center  space-x-4 border-2 border-purple-400 ">
             <a
               href="www.linkedin.com"
               className="text-blue-600 hover:text-blue-400 transition"
@@ -121,6 +144,9 @@ export default function Header() {
           </div>
           {/* SOCIAL MEDIA LINKS IN ONE DIV */}
         </div>
+        <button className="lg:hidden border-2 border-purple-400  rounded-md bg-[#eed814] my-3 hover:bg-[#afa016] transition-colors duration-1000 px-2 py-1 text-black text-[20px] font-bold">
+          <Link to="/team">Our Team</Link>
+        </button>
       </div>
     </nav>
   );
